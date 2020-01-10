193fb028eaf84a963429fac99cfbc0bf8bf98b67
Hicham Dioury <diouryhicham@gmail.com>
2020-01-10 12:53:56 -0700

    Initial commit of my project

---
+++ Gofish.cc
@@ -0,0 +1,19 @@
+//CPSC27200
+//Authors : my group
+//Date : Jan. 08 , 2020
+//File : Gofish.cpp
+// This is the class definition file.
+
+#include"Gofish.h"
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+//adding some comment
+
+using namespace std;
+
+void HelloWorld::printGreeting()
+{
+std::  cout <<"HELLO" <<std::endl ;
+
+}
---
+++ Gofish.h
@@ -0,0 +1,18 @@
+//CPSC27200
+//Authors : my group
+//Date : Jan. 08 , 2020
+// This is the class definition file.
+
+#ifndef GOFISH_H
+#define GOFISH_H
+
+using namespace std ;
+
+class HelloWorld
+{
+public:
+  void printGreeting();
+
+};
+
+#endif
